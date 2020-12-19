c2847872dd38d1b4c71f89b0b9f4c4079f7070ef
ryotaro <itour433@yahoo.co.jp>
2020-12-19 17:55:00 +0900

    カウントした数字を記憶する機能を実装（アプリを終了させてもデータが残る）

--- app/src/main/java/com/mou78/onenotcount/MainActivity.java
+++ app/src/main/java/com/mou78/onenotcount/MainActivity.java
@@ -2,6 +2,8 @@ package com.mou78.onenotcount;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
@@ -11,6 +13,8 @@ public class MainActivity extends AppCompatActivity {
     TextView textView;
     TextView saido;
     int count;
+    int setCount;
+    SharedPreferences pref;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,126 +23,215 @@ public class MainActivity extends AppCompatActivity {
 
         textView = (TextView)findViewById(R.id.count);
         saido = (TextView)findViewById(R.id.saido);
-        count = 0;
+//        count = 0;
+
+//        SharedPreferences pref= getSharedPreferences("countbox", Context.MODE_PRIVATE);
+        pref = getSharedPreferences("countbox",MODE_PRIVATE);
+        int setCount = pref.getInt("number",0);
+        textView.setText(setCount +"");
+//        textView.setText(pref.getInt("number" , 0));
+//////        String name = pref.getInt("number" ,0);
+//        int intValue = pref.getInt("number" , 0);
+//        textView.setText(pref.getInt("number" , 0));
+
+//        pref = getSharedPreferences("countbox", MODE_PRIVATE);
+//        SharedPreferences.Editor editor = pref.edit();
+
 
     }
 
     public void one(View v){
-        count = count + 1;
-        textView.setText(count +"");
+        int setCount = pref.getInt("number",0);
+        setCount = setCount + 1;
+        SharedPreferences.Editor editor = pref.edit();
+        SharedPreferences pref= getSharedPreferences("countbox", Context.MODE_PRIVATE);
+        editor.putInt("number", setCount);
+        editor.commit();
 
-        if (count <= 30) {
+//        int setCount = pref.getInt("number",0);
+        textView.setText(setCount +"");
+
+
+        if (setCount <= 30) {
             saido.setText("カウントしよう！");
-        } else if(count >= 100) {
+        } else if(setCount >= 100) {
             saido.setText("めっちゃ増えた！！！");
-        } else if(count >= 50) {
+        } else if(setCount >= 50) {
             saido.setText("50を超えた！！！");
-        } else if(count >= 30) {
+        } else if(setCount >= 30) {
         saido.setText("30を超えた！！！");
         }
     }
 
     public  void minus(View v) {
-        count = count - 1;
-        textView.setText(count + "");
-        if (count <= 30) {
+        int setCount = pref.getInt("number",0);
+        setCount = setCount - 1;
+        SharedPreferences.Editor editor = pref.edit();
+        SharedPreferences pref= getSharedPreferences("countbox", Context.MODE_PRIVATE);
+        editor.putInt("number", setCount);
+        editor.commit();
+
+//        int setCount = pref.getInt("number",0);
+        textView.setText(setCount +"");
+
+
+        if (setCount <= 30) {
             saido.setText("カウントしよう！");
-            } else if (count >= 100) {
-                saido.setText("めっちゃ増えた！！！");
-            } else if (count >= 50) {
-                saido.setText("50を超えた！！！");
-            } else if (count >= 30) {
-                saido.setText("30を超えた！！！");
+            } else if(setCount >= 100) {
+            saido.setText("めっちゃ増えた！！！");
+            } else if(setCount >= 50) {
+            saido.setText("50を超えた！！！");
+            } else if(setCount >= 30) {
+            saido.setText("30を超えた！！！");
             }
         }
     public void two(View v){
-        count = count +2;
-        textView.setText(count +"");
-            if (count <= 30) {
-                saido.setText("カウントしよう！");
-            } else if(count >= 100) {
+        int setCount = pref.getInt("number",0);
+        setCount = setCount + 2;
+        SharedPreferences.Editor editor = pref.edit();
+        SharedPreferences pref= getSharedPreferences("countbox", Context.MODE_PRIVATE);
+        editor.putInt("number", setCount);
+        editor.commit();
+
+//        int setCount = pref.getInt("number",0);
+        textView.setText(setCount +"");
+
+
+        if (setCount <= 30) {
+            saido.setText("カウントしよう！");
+            } else if(setCount >= 100) {
                 saido.setText("めっちゃ増えた！！！");
-            } else if(count >= 50) {
+            } else if(setCount >= 50) {
                 saido.setText("50を超えた！！！");
-            } else if(count >= 30) {
+            } else if(setCount >= 30) {
                 saido.setText("30を超えた！！！");
             }
         }
 
     public void three(View v){
-        count = count +3;
-        textView.setText(count +"");
-            if (count <= 30) {
-                saido.setText("カウントしよう！");
-            } else if(count >= 100) {
+        int setCount = pref.getInt("number",0);
+        setCount = setCount + 3;
+        SharedPreferences.Editor editor = pref.edit();
+        SharedPreferences pref= getSharedPreferences("countbox", Context.MODE_PRIVATE);
+        editor.putInt("number", setCount);
+        editor.commit();
+
+//        int setCount = pref.getInt("number",0);
+        textView.setText(setCount +"");
+
+
+        if (setCount <= 30) {
+            saido.setText("カウントしよう！");
+            } else if(setCount >= 100) {
                 saido.setText("めっちゃ増えた！！！");
-            } else if(count >= 50) {
+            } else if(setCount >= 50) {
                 saido.setText("50を超えた！！！");
-            } else if(count >= 30) {
+            } else if(setCount >= 30) {
                 saido.setText("30を超えた！！！");
             }
         }
 
     public void four(View v){
-        count = count +4;
-        textView.setText(count +"");
-            if (count <= 30) {
-                saido.setText("カウントしよう！");
-            } else if(count >= 100) {
+        int setCount = pref.getInt("number",0);
+        setCount = setCount + 4;
+        SharedPreferences.Editor editor = pref.edit();
+        SharedPreferences pref= getSharedPreferences("countbox", Context.MODE_PRIVATE);
+        editor.putInt("number", setCount);
+        editor.commit();
+
+//        int setCount = pref.getInt("number",0);
+        textView.setText(setCount +"");
+
+
+        if (setCount <= 30) {
+            saido.setText("カウントしよう！");
+            } else if(setCount >= 100) {
                 saido.setText("めっちゃ増えた！！！");
-            } else if(count >= 50) {
+            } else if(setCount >= 50) {
                 saido.setText("50を超えた！！！");
-            } else if(count >= 30) {
+            } else if(setCount >= 30) {
                 saido.setText("30を超えた！！！");
             }
         }
 
     public void minustwo(View v){
-        count = count -2;
-        textView.setText(count +"");
-            if (count <= 30) {
-                saido.setText("カウントしよう！");
-            } else if(count >= 100) {
+        int setCount = pref.getInt("number",0);
+        setCount = setCount - 2;
+        SharedPreferences.Editor editor = pref.edit();
+        SharedPreferences pref= getSharedPreferences("countbox", Context.MODE_PRIVATE);
+        editor.putInt("number", setCount);
+        editor.commit();
+
+//        int setCount = pref.getInt("number",0);
+        textView.setText(setCount +"");
+
+
+        if (setCount <= 30) {
+            saido.setText("カウントしよう！");
+            } else if(setCount >= 100) {
                 saido.setText("めっちゃ増えた！！！");
-            } else if(count >= 50) {
+            } else if(setCount >= 50) {
                 saido.setText("50を超えた！！！");
-            } else if(count >= 30) {
+            } else if(setCount >= 30) {
                 saido.setText("30を超えた！！！");
             }
         }
 
     public void minusfour(View v){
-        count = count -4;
-        textView.setText(count +"");
-            if (count <= 30) {
-                saido.setText("カウントしよう！");
-            } else if(count >= 100) {
+        int setCount = pref.getInt("number",0);
+        setCount = setCount - 4;
+        SharedPreferences.Editor editor = pref.edit();
+        SharedPreferences pref= getSharedPreferences("countbox", Context.MODE_PRIVATE);
+        editor.putInt("number", setCount);
+        editor.commit();
+
+//        int setCount = pref.getInt("number",0);
+        textView.setText(setCount +"");
+
+
+        if (setCount <= 30) {
+            saido.setText("カウントしよう！");
+            } else if(setCount >= 100) {
                 saido.setText("めっちゃ増えた！！！");
-            } else if(count >= 50) {
+            } else if(setCount >= 50) {
                 saido.setText("50を超えた！！！");
-            } else if(count >= 30) {
+            } else if(setCount >= 30) {
                 saido.setText("30を超えた！！！");
             }
         }
 
     public void minusthree(View v){
-        count = count -2;
-        textView.setText(count +"");
-            if (count <= 30) {
-                saido.setText("カウントしよう！");
-            } else if(count >= 100) {
+        int setCount = pref.getInt("number",0);
+        setCount = setCount - 2;
+        SharedPreferences.Editor editor = pref.edit();
+        SharedPreferences pref= getSharedPreferences("countbox", Context.MODE_PRIVATE);
+        editor.putInt("number", setCount);
+        editor.commit();
+
+//        int setCount = pref.getInt("number",0);
+        textView.setText(setCount +"");
+
+
+        if (setCount <= 30) {
+            saido.setText("カウントしよう！");
+            } else if(setCount >= 100) {
                 saido.setText("めっちゃ増えた！！！");
-            } else if(count >= 50) {
+            } else if(setCount >= 50) {
                 saido.setText("50を超えた！！！");
-            } else if(count >= 30) {
+            } else if(setCount >= 30) {
                 saido.setText("30を超えた！！！");
             }
         }
 
     public void delete(View v){
-        count = 0;
-        textView.setText(count +"");
-            if (count <= 30) {
+        SharedPreferences pref= getSharedPreferences("countbox", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = pref.edit();
+        editor.clear();
+        editor.commit();
+
+        textView.setText(setCount +"");
+
+        if (count <= 30) {
                 saido.setText("カウントしよう！");
             } else if(count >= 100) {
                 saido.setText("めっちゃ増えた！！！");
